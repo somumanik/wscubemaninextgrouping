d683bcac86b6441c317ee072104a2ece13ec8c44
Your Namesomumanik <manikchowdhuri@gmail.com>
2025-05-05 12:01:15 +0530

    lhhl

--- src/app/(website)/frequently-questions/page.jsx
+++ src/app/(website)/frequently-questions/page.jsx
@@ -1,8 +1,34 @@
+"use client";
 import Link from 'next/link'
 import React from 'react'
+import { useState } from "react";
 import { MdKeyboardArrowRight } from "react-icons/md"
 
+const faqs = [
+    {
+        question: "What is your return policy?",
+        answer: "You can return any item within 30 days of purchase.",
+    },
+    {
+        question: "How long does shipping take?",
+        answer: "Shipping usually takes 3-5 business days.",
+    },
+    {
+        question: "Do you offer customer support?",
+        answer: "Yes, we offer 24/7 customer support via chat and email.",
+    },
+];
+
+
 export default function FAQ() {
+
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const toggleFAQ = (index) => {
+        setActiveIndex(index === activeIndex ? null : index);
+    };
+
+
     return (
         <>
             <div className='flex flex-col justify-center items-center pt-12'>
@@ -15,9 +41,34 @@ export default function FAQ() {
                 </div>
 
             </div>
+            <div className='max-w-[1320px] mx-auto border-b-1 border-b-gray-200 border-solid py-5 '></div>
             <div className='max-w-[1320px] mx-auto '>
-                <div className='max-w-[100%] border-b-1 border-b-gray-200 border-solid py-5 '></div>
-                
+                <div className='max-w-[100%] border-b-1 border-b-gray-200 border-solid py-5 '>
+                    <div className=" p-4">
+                       
+
+                        {faqs.map((faq, index) => (
+                            <div key={index} className="mb-3 border rounded">
+                                <button
+                                    onClick={() => toggleFAQ(index)}
+                                    className={`w-full px-4 py-3 flex justify-between items-center text-left font-medium transition ${activeIndex === index ? "bg-gray-200" : "bg-gray-100"
+                                        }`}
+                                >
+                                    <span>{faq.question}</span>
+                                    <span>{activeIndex === index ? "−" : "+"}</span>
+                                </button>
+
+                                <div
+                                    className={`px-4 overflow-hidden transition-all duration-300 text-gray-700 bg-white ${activeIndex === index ? "max-h-40 py-2" : "max-h-0"
+                                        }`}
+                                >
+                                    <p>{faq.answer}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
             </div>
 
 
