46a77e2b6ba29085fcfdd14331a985d001831971
Your Namesomumanik <manikchowdhuri@gmail.com>
2025-05-05 11:07:47 +0530

    skkh

--- src/app/(website)/contact-us/mapview/MapView.jsx
+++ src/app/(website)/contact-us/mapview/MapView.jsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import { useEffect } from "react";
+import "leaflet/dist/leaflet.css";
 
 export default function MapView() {
     useEffect(() => {
-        // Leaflet icon fix
         const L = require("leaflet");
         delete L.Icon.Default.prototype._getIconUrl;
         L.Icon.Default.mergeOptions({
@@ -17,11 +16,8 @@ export default function MapView() {
     }, []);
 
     return (
-        <MapContainer center={[28.6139, 77.2090]} zoom={13} scrollWheelZoom={false} className="h-full w-full">
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+        <MapContainer center={[28.6139, 77.2090]} zoom={13} className="h-[500px] w-full">
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             <Marker position={[28.6139, 77.2090]}>
                 <Popup>Delhi, India</Popup>
             </Marker>
