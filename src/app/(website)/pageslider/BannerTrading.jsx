9691ffa0558f56634bfdda51cc798a152da426d8
Your Namesomumanik <manikchowdhuri@gmail.com>
2025-05-06 15:31:34 +0530

    bg

--- src/app/(website)/pageslider/BannerTrading.jsx
+++ src/app/(website)/pageslider/BannerTrading.jsx
@@ -3,12 +3,19 @@ import React from 'react'
 export default function BannerTrading() {
     return (
         <>
-            <div className=" my-10 bg-[url('https://wscubetech.co/Assignments/furniture/storage/app/public/uploads/images/home-page/e9234fa4-3ff6-4a6e-a00e-0c9ff26e7b20-1670180400.jpg')] bg-cover bg-center h-127 w-full text-white ">
-                <h1 className="px-[200px] pt-[150px] text-6xl  text-black">
-                New Trending Collection
-                </h1>
-                <p className='px-[200px] py-2 text-[18px]  text-gray-500'>We Believe That Good Design is Always in Season</p>
-                <button className='mx-[200px] my-15 px-5 py-2 text-[18px] border border-[#C09578] border-solid hover:bg-[#C09578] text-[#C09578] hover:text-white cursor-pointer' >SHOPPING NOW</button>
+            <div className="group my-10 cursor-pointer
+                bg-[url('https://wscubetech.co/Assignments/furniture/storage/app/public/uploads/images/home-page/e9234fa4-3ff6-4a6e-a00e-0c9ff26e7b20-1670180400.jpg')] bg-cover bg-center h-127 w-full text-white transition-all duration-300">
+                <div className="px-[200px] pt-[150px] transform transition-transform duration-300 group-hover:scale-110">
+                    <h1 className="text-6xl text-black">
+                        New Trending Collection
+                    </h1>
+                    <p className='py-2 text-[18px] text-gray-500'>
+                        We Believe That Good Design is Always in Season
+                    </p>
+                    <button className='mt-4 p-2 text-[18px] border border-[#C09578] border-solid hover:bg-[#C09578] text-[#C09578] hover:text-white cursor-pointer'>
+                        SHOPPING NOW
+                    </button>
+                </div>
             </div>
 
 
